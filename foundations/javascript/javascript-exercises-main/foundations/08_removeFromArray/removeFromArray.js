b5d0965c805b0b9c1cd948aacbb3d67a7599a74c
echu18 <elizabethchu18@gmail.com>
2025-12-27 00:29:14 +0800

    Add js foundation exercises

--- foundations/javascript/javascript-exercises-main/foundations/08_removeFromArray/removeFromArray.js
+++ foundations/javascript/javascript-exercises-main/foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  for (let i = 0; i < args.length; i++) {
+    while (array.includes(args[i])){
+      let idx = array.indexOf(args[i])
+      array.splice(idx, 1);
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
