bc55560c3c14f997cd184bc3942bf06c1eb1c0b9
echu18 <elizabethchu18@gmail.com>
2025-12-27 02:14:51 +0800

    Add the rest of foundations js exercises

--- foundations/javascript/javascript-exercises-main/foundations/16_findTheOldest/findTheOldest.js
+++ foundations/javascript/javascript-exercises-main/foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  people.forEach(function(person) {
+    if (!person.yearOfDeath) {
+      person.age = new Date().getFullYear() - person.yearOfBirth
+    } else {
+      person.age = person.yearOfDeath - person.yearOfBirth;
+    }
+  })
 
+  let max = 0;
+  let oldest;
+
+  people.forEach(function(person) {
+
+    if (person.age > max) {
+      max = person.age;
+      oldest = person;
+    }
+  })
+
+  return oldest;
 };
 
 // Do not edit below this line
